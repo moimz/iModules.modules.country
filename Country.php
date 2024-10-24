348fd107351abfb7941b0939898024ab171f7a90
arzzcom <arzz@arzz.com>
2024-10-24 13:59:59 +0900

    기본구조 추가

---
+++ Country.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 이 파일은 아이모듈 국가모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 국가모듈 클래스를 정의한다.
+ *
+ * @file /modules/country/Country.php
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2024. 10. 24.
+ */
+namespace modules\member;
+class Country
+{
+}
